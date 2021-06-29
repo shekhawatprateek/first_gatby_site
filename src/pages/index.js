49979d854443e22968fcc5f8e6341596bab475c0
Prateek Shekhawat <42514381+shekhawatprateek@users.noreply.github.com>
2021-06-29 18:17:12 +0530

    change in prateek_test branch

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,29 +1,14 @@
-import * as React from "react"
-import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
-
-import Layout from "../components/layout"
-import Seo from "../components/seo"
-
-const IndexPage = () => (
-  <Layout>
-    <Seo title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["AUTO", "WEBP", "AVIF"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
-    <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-    </p>
-  </Layout>
-)
-
+// Step 1: Import React
+import * as React from 'react'
+// Step 2: Define your component
+const IndexPage = () => {
+  return (
+    <main>
+      <title>Home Page</title>
+      <h1>Welcome to my Gatsby site!</h1>
+      <p>I'm making this by following the Gatsby Tutorial.</p>
+    </main>
+  )
+}
+// Step 3: Export your component
 export default IndexPage
